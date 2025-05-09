6cec2f390a313cf92a2d2143a6238badd8840ef1
douglas bortoleto <dgbortoleto>
2025-05-09 10:17:14 -0300

    Add controllers methods

--- app/Http/Controllers/Api/BalanceController.php
+++ app/Http/Controllers/Api/BalanceController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\AccountService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BalanceController extends Controller
 {
-    //
+    public function __construct(private AccountService $accountService) {}
+
+    public function index(Request $request)
+    {
+        $accountId = $request->query('account_id');
+        $balance = $this->accountService->get($accountId);
+
+        return $balance === null
+            ? response('0', Response::HTTP_NOT_FOUND)
+            : response($balance, Response::HTTP_OK);
+    }
 }
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,9 +3,62 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\TransactionService;
+use App\Services\AccountService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class EventController extends Controller
 {
-    //
+    public function __construct(
+        private TransactionService $transactionService,
+        private AccountService $accountService
+    ) {}
+
+    public function store(Request $request)
+    {
+        return match ($request->input('type')) {
+            'deposit' => response()->json(
+                $this->transactionService->deposit(
+                    $request->input('destination'),
+                    $request->input('amount')
+                ),
+                Response::HTTP_CREATED
+            ),
+            'withdraw' => $this->handleWithdraw($request),
+            'transfer' => $this->handleTransfer($request),
+            default => response(null, Response::HTTP_BAD_REQUEST),
+        };
+    }
+
+    private function handleWithdraw(Request $request)
+    {
+        $result = $this->transactionService->withdraw(
+            $request->input('origin'),
+            $request->input('amount')
+        );
+
+        return $result === 0
+            ? response('0', Response::HTTP_NOT_FOUND)
+            : response()->json($result, Response::HTTP_CREATED);
+    }
+
+    private function handleTransfer(Request $request)
+    {
+        $result = $this->transactionService->transfer(
+            $request->input('origin'),
+            $request->input('destination'),
+            $request->input('amount')
+        );
+
+        return $result === 0
+            ? response('0', Response::HTTP_NOT_FOUND)
+            : response()->json($result, Response::HTTP_CREATED);
+    }
+
+    public function reset()
+    {
+        $this->accountService->reset();
+        return response('', Response::HTTP_OK);
+    }
 }
